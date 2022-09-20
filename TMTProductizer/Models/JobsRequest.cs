900c71a09cc5bc3d03425e34c0697e27073ab7ab
lassi.patanen <lassi.patanen@gofore.com>
2022-09-20 09:31:21 +0300

    Added location query model and renamed keywords property to query

--- TMTProductizer/Models/JobsRequest.cs
+++ TMTProductizer/Models/JobsRequest.cs
@@ -2,6 +2,13 @@ namespace TMTProductizer.Models;
 
 public class JobsRequest
 {
-    public string Keywords { get; set; }
-    public string Location { get; set; }
+    public string Query { get; set; }
+    public LocationQuery LocationQuery { get; set; }
+}
+
+public class LocationQuery
+{
+    public IEnumerable<string> Countries { get; set; }
+    public IEnumerable<string> Regions { get; set; }
+    public IEnumerable<string> Municipalities { get; set; }
 }
