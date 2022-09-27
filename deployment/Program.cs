803bfc1e9bcc8d1c10d3cccd51a28d4533688ef6
lassi.patanen <lassi.patanen@gofore.com>
2022-09-27 15:21:44 +0300

    created setup for required resources to run productizer in AWS

--- deployment/Program.cs
+++ deployment/Program.cs
@@ -1,15 +1,3 @@
-﻿using Pulumi;
-using Pulumi.Aws.S3;
-using System.Collections.Generic;
+﻿using Deployment.TmtProductizerStack;
 
-return await Deployment.RunAsync(() =>
-{
-   // Create an AWS resource (S3 Bucket)
-   var bucket = new Bucket("my-bucket");
-
-   // Export the name of the bucket
-   return new Dictionary<string, object?>
-   {
-      ["bucketName"] = bucket.Id
-   };
-});
+return await Pulumi.Deployment.RunAsync<TmtProductizerStack>();
---
+++ deployment/TmtProductizerStack.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using Pulumi;
+using Pulumi.Aws.Iam;
+using Pulumi.Aws.Lambda;
+using Pulumi.Aws.Lambda.Inputs;
+using Pulumi.Command.Local;
+
+namespace Deployment.TmtProductizerStack;
+
+public class TmtProductizerStack : Stack
+{
+    public TmtProductizerStack()
+    {
+        var role = new Role("lambdaRole", new RoleArgs
+        {
+            AssumeRolePolicy = JsonSerializer.Serialize(new Dictionary<string, object?>
+            {
+                { "Version", "2012-10-17" },
+                {
+                    "Statement", new[]
+                    {
+                        new Dictionary<string, object?>
+                        {
+                            { "Action", "sts:AssumeRole" },
+                            { "Effect", "Allow" },
+                            { "Sid", "" },
+                            {
+                                "Principal", new Dictionary<string, object?>
+                                {
+                                    { "Service", "lambda.amazonaws.com" }
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+        });
+
+        var rolePolicyAttachment = new RolePolicyAttachment("lambdaRoleAttachment", new RolePolicyAttachmentArgs
+        {
+            Role = Output.Format($"{role.Name}"),
+            PolicyArn = ManagedPolicy.AWSLambdaBasicExecutionRole.ToString()
+        });
+
+        var lambdaFunction = new Function("lambdaFunction", new FunctionArgs
+        {
+            Role = role.Arn,
+            Runtime = "dotnet6",
+            Handler = "TMTProductizer",
+            Timeout = 15,
+            Environment = new FunctionEnvironmentArgs
+            {
+                Variables =
+                {
+                    { "ASPNETCORE_ENVIRONMENT", "Development" }
+                }
+            },
+            Code = new AssetArchive(new Dictionary<string, AssetOrArchive>
+            {
+                { "folder", new FileArchive("../TMTProductizer") }
+            })
+        });
+
+        var functionUrl = new FunctionUrl("lambdaFunctionUrl", new FunctionUrlArgs
+        {
+            FunctionName = lambdaFunction.Arn,
+            AuthorizationType = "NONE"
+        });
+
+        var localCommand = new Command("addPermissions", new CommandArgs
+        {
+            Create = Output.Format(
+                $"aws lambda add-permission --function-name {lambdaFunction.Arn} --action lambda:InvokeFunctionUrl --principal '*' --function-url-auth-type NONE --statement-id FunctionUrlAllowAccess")
+        }, new CustomResourceOptions
+        {
+            DeleteBeforeReplace = true,
+            DependsOn = new InputList<Resource>
+            {
+                lambdaFunction
+            }
+        });
+
+
+        Url = functionUrl.FunctionUrlResult;
+    }
+
+    [Output] private Output<string> Url { get; }
+}
