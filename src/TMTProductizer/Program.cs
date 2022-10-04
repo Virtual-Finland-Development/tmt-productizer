c37dfe48625a2c8bda86dd127c22523d911d2e57
lassi.patanen <lassi.patanen@gofore.com>
2022-10-04 16:31:32 +0300

    Changed endpoint route to match the route on testbed

--- src/TMTProductizer/Program.cs
+++ src/TMTProductizer/Program.cs
@@ -36,7 +36,7 @@ if (app.Environment.IsDevelopment())
 
 app.Urls.Add("http://*:80");
 
-app.MapPost("/jobs", async (JobsRequest requestModel, [FromServices] IJobService service) =>
+app.MapPost("/test/lassipatanen/Job/JobPosting", async (JobsRequest requestModel, [FromServices] IJobService service) =>
     {
         IReadOnlyList<Job> jobs;
         try
