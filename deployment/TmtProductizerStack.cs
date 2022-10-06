40667ce588130bb4c156f049fac0fd4c601fe796
lassi.patanen <lassi.patanen@gofore.com>
2022-10-06 08:43:20 +0300

    Update artifact path in Pulumi stack

--- deployment/TmtProductizerStack.cs
+++ deployment/TmtProductizerStack.cs
@@ -56,7 +56,7 @@ public class TmtProductizerStack : Stack
                     { "ASPNETCORE_ENVIRONMENT", "Development" }
                 }
             },
-            Code = new FileArchive("/home/runner/work/tmt-productizer/tmt-productizer/deployment/release/Application_Artifact.zip")
+            Code = new FileArchive("release/Application_Artifact.zip")
         });
 
         var functionUrl = new FunctionUrl("tmt-productizer-function-url", new FunctionUrlArgs
