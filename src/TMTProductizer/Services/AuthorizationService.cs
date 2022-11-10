292f1ed9ba8a5f76c8936c5e4bc36d580ad185f5
Lassi Piironen <lsipii@kapsi.fi>
2022-11-10 19:06:00 +0200

    feat: add origin check

--- src/TMTProductizer/Services/AuthorizationService.cs
+++ src/TMTProductizer/Services/AuthorizationService.cs
@@ -13,11 +13,18 @@ public class AuthorizationService : IAuthorizationService
     {
         // Get the auth headers from the origin request
         var originHeaders = request.Headers.ToDictionary(x => x.Key.ToLowerInvariant(), x => x.Value.ToString());
-        if (!originHeaders.ContainsKey("authorization") || !originHeaders.ContainsKey("x-authorization-provider")) {
-            throw new HttpRequestException(); // Throws 401 if no auth headers.
+        
+        // Check CORS origin against allowed origins
+        if (!originHeaders.TryGetValue("origin", out var origin) || !IsAllowedOrigin(origin))
+        {
+            throw new HttpRequestException("Unknown origin", null, System.Net.HttpStatusCode.Unauthorized); // Throws 401 if bad origin
         }
 
         // Prep authorization request
+        if (!originHeaders.ContainsKey("authorization") || !originHeaders.ContainsKey("x-authorization-provider")) {
+            throw new HttpRequestException("Missing headers", null, System.Net.HttpStatusCode.Unauthorized); // Throws 401 if no auth headers
+        }
+
         var authorizeRequest = new HttpRequestMessage() {
             RequestUri = new Uri($"{_client.BaseAddress}/authorize"),
             Method = HttpMethod.Post,
@@ -31,9 +38,17 @@ public class AuthorizationService : IAuthorizationService
         // Engage
         var response = await _client.SendAsync(authorizeRequest); 
         if (!response.IsSuccessStatusCode) {
-            throw new HttpRequestException(); // Throw 401 if not authorized.
+            throw new HttpRequestException("Access Denied", null, System.Net.HttpStatusCode.Unauthorized); // Throw 401 if not authorized.
         }
 
         return null;
+    }
+
+    /**
+     * Check if the origin is allowed.
+     */
+    private bool IsAllowedOrigin(string origin)
+    {
+        return origin == "https://gateway.testbed.fi";
     }
 }
