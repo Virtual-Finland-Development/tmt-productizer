40cd99c48d7cb20c7a78fd5f2603f98f4a3bf580
Lassi Piironen <lsipii@kapsi.fi>
2022-11-15 12:29:40 +0200

    feat: add a testcase for authorization service

---
+++ src/TMTProductizer.UnitTests/Services/AuthorizationServiceTests.cs
@@ -0,0 +1,73 @@
+using System.Net;
+using TMTProductizer.Services;
+using Moq;
+using Moq.Protected;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+
+namespace TMTProductizer.UnitTests.Services;
+
+public class AuthorizationServiceTests
+{
+    /**
+    * Mockup service for testing
+    */
+    private IAuthorizationService getAuthorizationServiceMock() {
+        var handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+        handler.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.Unauthorized,
+                Content = new StringContent("")
+            });
+        var httpClient = new HttpClient(handler.Object) { BaseAddress = new Uri("http://localhost/") };
+        var authorizationService = new AuthorizationService(httpClient);
+
+        return authorizationService;
+    }
+
+    [Test]
+    public void TryingToAuthorize_BadHeaders_ReturnsAccessDeniedException()
+    {
+        // Test bad headers
+        Action badHeadersAction = () => {
+            var httpContext = new DefaultHttpContext {
+                Request = {
+                    Headers = {
+                        { "Authorization", "Bearer: fake_token" },
+                    }
+                }
+            };
+            var authorizationService = this.getAuthorizationServiceMock();
+            authorizationService.Authorize(httpContext.Request).Wait();
+        };
+        badHeadersAction.Should().Throw<HttpRequestException>()
+            .WithMessage("Missing headers");
+
+    }
+
+    [Test]
+    public void TryingToAuthorize_InvalidHeaders_ReturnsAccessDeniedException()
+    {
+        // Test good, but invalid headers
+        Action invalidHeadersAction = () => {
+            var httpContext = new DefaultHttpContext {
+                Request = {
+                    Headers = {
+                        { "Authorization", "Bearer abba-bubba-cobra" },
+                        { "X-Authorization-Provider", "fake_provider" },
+                    }
+                }
+            };
+
+            var authorizationService = this.getAuthorizationServiceMock();
+            authorizationService.Authorize(httpContext.Request).Wait();
+        };
+        invalidHeadersAction.Should().Throw<HttpRequestException>()
+            .WithMessage("Access Denied");
+    }
+}
