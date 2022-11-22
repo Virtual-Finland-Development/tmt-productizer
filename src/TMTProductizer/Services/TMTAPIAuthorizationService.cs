57064e23ec955cdf3c86ef1b9b9ad580545bb352
Lassi Piironen <lsipii@kapsi.fi>
2022-11-22 20:38:32 +0200

    refactor: samefy the date utils namespace to the other utils

--- src/TMTProductizer/Services/TMTAPIAuthorizationService.cs
+++ src/TMTProductizer/Services/TMTAPIAuthorizationService.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Text.Json;
 using TMTProductizer.Models;
 using TMTProductizer.Services.AWS;
-using TMTProductizer.Utils.DateUtils;
+using TMTProductizer.Utils;
 
 namespace TMTProductizer.Services;
 
