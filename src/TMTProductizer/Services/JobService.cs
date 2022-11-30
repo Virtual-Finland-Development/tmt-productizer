8f2da662f7a922c8777115ab587945fe651584b9
Lassi Piironen <lsipii@kapsi.fi>
2022-11-30 15:18:20 +0200

    draf: paging and filtering

--- src/TMTProductizer/Services/JobService.cs
+++ src/TMTProductizer/Services/JobService.cs
@@ -32,25 +32,13 @@ public class JobService : IJobService
         var jobs = TransformTMTResultsToJobs(results);
 
         // Filter and paginate the results
-        // @TODO: Filtering and pagination needs to be implemented
-        var jobsTotalCount = jobs.Count;
-        if (query.Query != "")
-        {
-            jobs = jobs.FindAll(j =>
-            {
-                // The ! operator does not seem to work at runtime, like it says: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-forgiving
-                // Use null-comparison instead
-                return (j.BasicInfo.Description != null && j.BasicInfo.Description.Contains(query.Query)) || (j.BasicInfo.Title != null && j.BasicInfo.Title.Contains(query.Query));
-            });
-        }
-
-        return (jobs, jobsTotalCount);
+        return FilterAndPaginateJobs(jobs, query);
     }
 
     /// <summary>
     /// Fetches the results from the cache or from the TMT API if the cache is empty.
     /// </summary>
-    private async Task<Hakutulos?> GetTMTAPIResultsFromCache()
+    private async Task<Hakutulos> GetTMTAPIResultsFromCache()
     {
         var cachedResults = await _tmtApiResultsCacheService.GetCacheItem<Hakutulos>(_tmtCacheKey);
         if (cachedResults != null)
@@ -61,7 +49,7 @@ public class JobService : IJobService
 
         _logger.LogInformation("Fetching TMT API results from TMT API");
         var results = await GetTMTResultsFromAPI();
-        if (results != null)
+        if (results.IlmoituksienMaara > 0)
         {
             _logger.LogInformation("Saving TMT API results to cache");
             await _tmtApiResultsCacheService.SaveCacheItem(_tmtCacheKey, results, _tmtCacheTTL);
@@ -73,7 +61,7 @@ public class JobService : IJobService
     /// <summary>
     /// Fetches the results from the TMT API
     /// </summary>
-    private async Task<Hakutulos?> GetTMTResultsFromAPI()
+    private async Task<Hakutulos> GetTMTResultsFromAPI()
     {
         // Get TMT Authorization Details
         APIAuthorizationPackage authorizationPackage = await _tmtApiAuthorizationService.GetAPIAuthorizationPackage(); // Throws HttpRequestException;
@@ -81,18 +69,33 @@ public class JobService : IJobService
         // Build a proxy client
         var httpClient = _clientFactory.GetProxyClient(authorizationPackage);
 
-        // @TODO: fetch all the pages with a loop, merge to a single Hakutulos object
-        Hakutulos? results = await GetTMTAPIResultPage(httpClient, authorizationPackage, 100, 0);
+        // Fetch all the pages with a loop, merge to a single Hakutulos object
+        var results = new Hakutulos(new List<Tyopaikkailmoitus>(), 0);
+        var pagingOffset = 0;
+        var pagingLimit = 400;
+        Hakutulos? pageResults = null;
 
+        do
+        {
+            pageResults = await GetTMTAPIResultPage(httpClient, authorizationPackage, pagingOffset, pagingLimit);
+            if (pageResults != null)
+            {
+                results.Ilmoitukset.AddRange(pageResults.Ilmoitukset);
+                pagingOffset = pagingOffset + pagingLimit;
+            }
+        } while (pageResults != null && pageResults.IlmoituksienMaara == pagingLimit); // have results and not be on the last page
+
+        results.IlmoituksienMaara = results.Ilmoitukset.Count;
         return results;
     }
 
     /// <summary>
     /// Fetches a single page of unfiltered results from the TMT API.
     /// </summary>
-    private async Task<Hakutulos?> GetTMTAPIResultPage(HttpClient httpClient, APIAuthorizationPackage authorizationPackage, int limit, int offset)
+    private async Task<Hakutulos?> GetTMTAPIResultPage(HttpClient httpClient, APIAuthorizationPackage authorizationPackage, int pagingOffset, int pagingLimit)
     {
-        var queryParamsString = "limit=" + limit + "&offset=" + offset;
+        var pageNumber = GetPageNumberFromOffsetAndLimit(pagingOffset, pagingLimit);
+        var queryParamsString = "maara=" + pagingLimit + "&sivu=" + pageNumber;
 
         // Form the request
         var requestMessage = new HttpRequestMessage
@@ -103,24 +106,26 @@ public class JobService : IJobService
         requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authorizationPackage.AccessToken);
 
         // Send request
+        _logger.LogInformation("Fetching TMT API results from TMT API, page: {pageNumber}, offset: {pagingLimit}, limit: {pagingLimit}", pageNumber, pagingOffset, pagingLimit);
         var response = await httpClient.SendAsync(requestMessage);
+        var responseAsString = await response.Content.ReadAsStringAsync();
         if (!response.IsSuccessStatusCode)
         {
             _logger.LogError("TMT API responded with: {StatusCode}", response.StatusCode);
-            _logger.LogError("Content: {Content}", await response.Content.ReadAsStringAsync());
+            _logger.LogError("Content: {Content}", responseAsString);
             return null;
         }
 
         try
         {
-            return await response.Content.ReadFromJsonAsync<Hakutulos>();
+            return StringUtils.JsonDeserializeObject<Hakutulos>(responseAsString);
         }
         catch (Exception e)
         {
-            if (offset == 0)
+            if (pageNumber == 0)
             {
                 _logger.LogError(e, "Failed to parse TMT API response");
-                await File.WriteAllTextAsync("TMTResponseOutputDebug.txt", await response.Content.ReadAsStringAsync());
+                await File.WriteAllTextAsync("TMTResponseOutputDebug.txt", responseAsString);
                 _logger.LogError("Wrote a response output debug file to TMTResponseOutputDebug.txt");
             }
         }
@@ -128,14 +133,19 @@ public class JobService : IJobService
         return null;
     }
 
+    private int GetPageNumberFromOffsetAndLimit(int pagingOffset, int pagingLimit)
+    {
+        var (quotient, _) = Math.DivRem(pagingOffset, pagingLimit);
+
+        return quotient;
+    }
+
     /// <summary>
     /// Transforms the TMT API results to a list of jobs.
     /// </summary>
-    private List<Job> TransformTMTResultsToJobs(Hakutulos? results)
+    private List<Job> TransformTMTResultsToJobs(Hakutulos results)
     {
         var jobs = new List<Job>();
-
-        if (results == null) return jobs;
 
         jobs.AddRange(results.Ilmoitukset.Select(ilmoitus => new Job
         {
@@ -158,5 +168,50 @@ public class JobService : IJobService
         }));
 
         return jobs;
+    }
+
+    /// <summary>
+    /// Filters and paginates the jobs.
+    /// </summary>
+    private (List<Job>, int) FilterAndPaginateJobs(List<Job> jobs, JobsRequest query)
+    {
+        int totalCount = jobs.Count;
+
+        // Filter by search phase
+        if (query.Query != "")
+        {
+            jobs = jobs.FindAll(j =>
+            {
+                // The ! operator does not seem to work at runtime, like it says: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-forgiving
+                // Use null-comparison instead
+                return (j.BasicInfo.Description != null && j.BasicInfo.Description.Contains(query.Query)) || (j.BasicInfo.Title != null && j.BasicInfo.Title.Contains(query.Query));
+            });
+        }
+
+        // Filter by location
+        if (query.Location.Municipalities.Any())
+        {
+            jobs = jobs.FindAll(j => query.Location.Municipalities.Contains(j.Location.Municipality));
+        }
+        if (query.Location.Regions.Any())
+        {
+            jobs = jobs.FindAll(j => query.Location.Regions.Contains(j.Location.Municipality)); // @TODO: implement region filtering, maybe pre-filter the results from the API
+        }
+        if (query.Location.Countries.Any())
+        {
+            jobs = jobs.FindAll(j => query.Location.Countries.Contains(j.Location.Municipality));
+        }
+
+        // Paginate the jobs
+        if (query.Paging.Offset != 0)
+        {
+            jobs = jobs.Skip(query.Paging.Offset).ToList();
+        }
+        if (query.Paging.Limit != 0)
+        {
+            jobs = jobs.Take(query.Paging.Limit).ToList();
+        }
+
+        return (jobs, totalCount);
     }
 }
