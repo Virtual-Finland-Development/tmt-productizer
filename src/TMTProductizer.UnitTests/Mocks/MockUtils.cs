2bb3042af85dd8efc43000c6d3828321f9f288f8
Lassi Piironen <lsipii@kapsi.fi>
2022-12-02 14:41:38 +0200

    fix: mockup raw data in unittests not referenced good for all build setups

--- src/TMTProductizer.UnitTests/Mocks/MockUtils.cs
+++ src/TMTProductizer.UnitTests/Mocks/MockUtils.cs
@@ -6,10 +6,12 @@ public static class MockUtils
 
     public static string GetTMTTestResponse()
     {
+        string mockDataFilepath = "./Mocks/testTMTResponse.json";
+
         try
         {
             // Open the text file using a stream reader.
-            using (var sr = new StreamReader("../../src/TMTProductizer.UnitTests/Mocks/testTMTResponse.json"))
+            using (var sr = new StreamReader(mockDataFilepath))
             {
                 // Read the stream to a string, and write the string to the console.
                 var line = sr.ReadToEnd();
