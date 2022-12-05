2e90bef1fb4e552e02be61a2d3c4b4920034911c
Lassi Piironen <lsipii@kapsi.fi>
2022-12-05 14:33:50 +0200

    fix: add ilmoitusID to the cache definition

--- src/TMTProductizer/Models/Cache/CachedTyopaikkailmoitus.cs
+++ src/TMTProductizer/Models/Cache/CachedTyopaikkailmoitus.cs
@@ -7,12 +7,20 @@ public class CachedTyopaikkailmoitus
 {
     public CachedTyopaikkailmoitus(Tyopaikkailmoitus tyopaikkailmoitus)
     {
+        IlmoituksenID = tyopaikkailmoitus.IlmoituksenID;
         Perustiedot = tyopaikkailmoitus.Perustiedot;
         Sijainti = tyopaikkailmoitus.Sijainti;
         IlmoittajanNimi = tyopaikkailmoitus.IlmoittajanNimi;
         Julkaisupvm = tyopaikkailmoitus.Julkaisupvm;
         Hakeminen = tyopaikkailmoitus.Hakeminen;
     }
+
+    /// <summary>
+    /// **fi:** Työpaikkailmoituksen tunniste | **en:** Unique ID of the job posting
+    /// </summary>
+    /// <value>**fi:** Työpaikkailmoituksen tunniste | **en:** Unique ID of the job posting</value>
+    [DataMember(Name = "ilmoituksenID", EmitDefaultValue = false)]
+    public string IlmoituksenID { get; set; }
 
     /// <summary>
     /// Gets or Sets Perustiedot
