81e9119d278714faf0367e817cd1ce46adb6e179
Lassi Piironen <lsipii@kapsi.fi>
2022-12-15 11:01:11 +0200

    refactor: remove bad merge leftover [skip ci]

--- src/TMTProductizer/Services/JobService.cs
+++ src/TMTProductizer/Services/JobService.cs
@@ -76,11 +76,23 @@ public class JobService : IJobService
         // Filter by search phase
         if (query.Query != "")
         {
+            var searchPhrases = query.Query.Split(',').ToList();
+
             results.Ilmoitukset = results.Ilmoitukset.FindAll(ilmoitus =>
             {
                 var title = ilmoitus.Perustiedot.TyonOtsikko.FirstOrDefault(x => x.KieliKoodi == requestedKielikoodi)?.Arvo.ToString() ?? string.Empty;
                 var description = ilmoitus.Perustiedot.TyonKuvaus.FirstOrDefault(x => x.KieliKoodi == requestedKielikoodi)?.Arvo.ToString() ?? string.Empty;
-                return (description != null && description.Contains(query.Query)) || (title != null && title.Contains(query.Query));
+
+                var isMatch = false;
+                foreach (var searchPhrase in searchPhrases)
+                {
+                    if ((description != null && description.Contains(searchPhrase)) || (title != null && title.Contains(searchPhrase)))
+                    {
+                        isMatch = true;
+                        break;
+                    }
+                }
+                return isMatch;
             });
         }
 
@@ -109,35 +121,5 @@ public class JobService : IJobService
         }
 
         return results;
-    }
-
-    /// <summary>
-    /// Transforms the TMT API results to a list of jobs.
-    /// </summary>
-    private List<Job> TransformTMTResultsToJobs(CachedHakutulos results)
-    {
-        var jobs = new List<Job>();
-
-        jobs.AddRange(results.Ilmoitukset.Select(ilmoitus => new Job
-        {
-            Employer = ilmoitus.IlmoittajanNimi.FirstOrDefault(x => x.KieliKoodi == "fi")?.Arvo.ToString() ?? string.Empty,
-            Location = new Location
-            {
-                Municipality = ilmoitus.Sijainti.Toimipaikka.Postitoimipaikka,
-                Postcode = ilmoitus.Sijainti.Toimipaikka.Postinumero
-            },
-            BasicInfo = new BasicInfo
-            {
-                Title = ilmoitus.Perustiedot.TyonOtsikko.FirstOrDefault(x => x.KieliKoodi == "fi")?.Arvo.ToString() ?? string.Empty,
-                Description =
-                    ilmoitus.Perustiedot.TyonKuvaus.FirstOrDefault(x => x.KieliKoodi == "fi")?.Arvo.ToString() ?? string.Empty,
-                WorkTimeType = ilmoitus.Perustiedot.TyoAika
-            },
-            PublishedAt = ilmoitus.Julkaisupvm,
-            ApplicationUrl = ilmoitus.Hakeminen.HakemuksenUrl,
-            ApplicationEndDate = ilmoitus.Hakeminen.HakuaikaPaattyy
-        }));
-
-        return jobs;
     }
 }
